54cf367eb3cc1c6ad3e95d4bce0865eb0b99b3e2
valendrenitos <smaniotto.valentin98@gmail.com>
2026-02-13 14:38:49 +0100

    update gitignore n SetDB for licenses

--- SetDB.py
+++ SetDB.py
@@ -8,6 +8,7 @@ import re
 ########################
 
 datamedia = pd.read_csv("data_media.csv")
+datalicenses=pd.read_csv("data_licenses.csv")
 datamedia["évènement"]=datamedia["évènement"].astype('str')
 datamedia["sport"]=datamedia["sport"].astype('str')
 datamedia["année"]=datamedia["année"].astype('float64')
@@ -24,8 +25,15 @@ conn=dbc.TryConnect()
 for i,row in datamedia.iterrows():
     quer="INSERT INTO media_table.media_data (sport_event,sport,year,avrg_tv_aud,avrg_tv_match,total_match,hours_live,numb_of_post) VALUES ('"+row["sport_event"]+"','"+row["sport"]+"','"+str(row["year"])+"','"+str(row["avrg_tv_aud"])+"','"+str(row["avrg_tv_match"])+"','"+str(row["total_match"])+"','"+str(row["hours_live"])+"','"+str(row["numb_of_post"])+"')"    
     print(quer)
+    #conn._execute_query(quer)
+    #conn.commit()
+
+
+for i,row in datalicenses.iterrows():
+    quer="INSERT INTO media_table.licenses (year, code_commune, nom_commune, num_dep, region, code_fed, nom_fed, total_lic, total_f, total_h, h_1_9, h_10_19, h_20_29,h_30_59, h_60_74, h_75,  f_1_9, f_10_19, f_20_29, f_30_59, f_60_74, f_75) VALUES ('"+str(row["annee"])+"','"+str(row["code_commune"])+"','"+str(row["nom_commune"])+"','"+str(row["num_departement"])+"','"+str(row["region"])+"','"+str(row["code_fed"])+"','"+str(row["nom_fed"])+"','"+str(row["total_lic"])+"','"+str(row["total_f"])+"','"+str(row["total_h"])+"','"+str(row["h_1_9"])+"','"+str(row["h_10_19"])+"','"+str(row["h_20_29"])+"','"+str(row["h_30_59"])+"','"+str(row["h_60_74"])+"','"+str(row["h_75"])+"','"+str(row["f_1_9"])+"','"+str(row["f_10_19"])+"','"+str(row["f_20_29"])+"','"+str(row["f_30_59"])+"','"+str(row["f_60_74"])+"','"+str(row["f_75"])+"')"   
+    print(quer)
     conn._execute_query(quer)
     conn.commit()
-
+  
 print(conn._execute_query("SELECT * FROM media_table.media_data"))
 dbc.CloseCon(conn)
