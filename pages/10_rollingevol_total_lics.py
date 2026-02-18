f351e9299f67b778ec70668ee23f99753fa73140
neronalissiane <neron.alissiane@gmail.com>
2026-02-18 22:41:16 +0100

    rolling average to fix tomorrow

---
+++ pages/10_rollingevol_total_lics.py
@@ -0,0 +1,40 @@
+rolling_avg = (
+    df.groupby(["nom_fed", "year"])["total_lic"].sum()
+).unstack("year").rolling(3, axis=1, min_periods=1).mean()
+
+df_long = (
+    rolling_avg
+    .mul(100)
+    .rename_axis("federation")
+    .reset_index()
+    .melt(id_vars="federation", var_name="annee", value_name="total lics")
+)
+
+df_long["progression"] = (
+    df_long.groupby("federation")["total_lic"]
+    .transform(lambda s: s - s.iloc[0])
+)
+
+fig = px.scatter(
+    df_long,
+    x="total lics",
+    y="progression",
+    animation_frame="annee",
+    animation_group="federation",
+    hover_name="federation",
+    labels={
+        "total lics": "Total licenciés",
+        "progression": "Progression depuis l'origine",
+        "annee": "Année"
+    },
+    title="Évolution des licenciés par fédération",
+    height=750
+)
+
+fig.add_vline(x=50, line_dash="dash", line_color="red", opacity=0.5)
+fig.add_hline(y=0, line_dash="dash", line_color="red", opacity=0.5)
+
+fig.update_xaxes(range=[0, 100])
+fig.update_yaxes(range=[-10, 35])
+
+st.plotly_chart(fig, use_container_width=True)
