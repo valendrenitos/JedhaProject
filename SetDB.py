2e43595a3efdcf3cc6f75848bc86166db1e5cd6e
unknown <smaniotto.valentin98@gmail.com>
2026-02-19 12:50:54 +0100

    updating db and list of column on app.py

--- SetDB.py
+++ SetDB.py
@@ -10,22 +10,23 @@ import re
 # data_clubs = data_clubs.drop(["indexation"],axis=1)
 # data_clubs=data_clubs.astype("float64")
 
-data_clubs2 = pd.read_csv("total_pred.csv")
-data_clubs2 = data_clubs2.drop(["index"],axis=1)
-data_clubs2=data_clubs2.astype("float64")
-#datamedia = pd.read_csv("data_media.csv")
+# data_clubs2 = pd.read_csv("total_pred.csv")
+# data_clubs2 = data_clubs2.drop(["index"],axis=1)
+# data_clubs2=data_clubs2.astype("float64")
+datamedia = pd.read_csv("data_media.csv")
 # datalicenses=pd.read_csv("data_licenses.csv")
-# datamedia["évènement"]=datamedia["évènement"].astype('str')
-# datamedia["sport"]=datamedia["sport"].astype('str')
-# datamedia["année"]=datamedia["année"].astype('float64')
-# datamedia["audience TV moynne total event"]=datamedia["audience TV moynne total event"].astype('float64')
-# datamedia["audience TV moy par match"]=datamedia["audience TV moy par match"].astype('float64')
-# datamedia["Nombre de matchs"]=datamedia["Nombre de matchs"].astype('float64')
-# datamedia["heures d'antenne (télévision linéaire)"]=datamedia["heures d'antenne (télévision linéaire)"].astype('float64')
-# datamedia["nombre de posts RS"]=datamedia["nombre de posts RS"].astype('float64')
-#datamedia=datamedia.rename(columns={"évènement": "sport_event", "année": "year","audience TV moynne total event": "avrg_tv_aud","audience TV moy par match": "avrg_tv_match", "Nombre de matchs": "total_match", "heures d'antenne (télévision linéaire)": "hours_live","nombre de posts RS": "numb_of_post"  })
+datamedia["évènement"]=datamedia["évènement"].astype('str')
+datamedia["sport"]=datamedia["sport"].astype('str')
+datamedia["année"]=datamedia["année"].astype('float64')
+datamedia["genre"]=datamedia["genre"].astype('str')
+datamedia["audience TV moynne total event"]=datamedia["audience TV moynne total event"].astype('float64')
+datamedia["audience TV moy par match"]=datamedia["audience TV moy par match"].astype('float64')
+datamedia["Nombre de matchs"]=datamedia["Nombre de matchs"].astype('float64')
+datamedia["heures d'antenne (télévision linéaire)"]=datamedia["heures d'antenne (télévision linéaire)"].astype('float64')
+datamedia["nombre de posts RS"]=datamedia["nombre de posts RS"].astype('float64')
+datamedia=datamedia.rename(columns={"évènement": "sport_event", "année": "year","audience TV moynne total event": "avrg_tv_aud","audience TV moy par match": "avrg_tv_match", "Nombre de matchs": "total_match", "heures d'antenne (télévision linéaire)": "hours_live","nombre de posts RS": "numb_of_post"  })
 
-#datamedia = datamedia.replace({float('nan'): None})
+datamedia = datamedia.replace({float('nan'): None})
 # datalicenses=datalicenses.drop(["code_commune","nom_commune","num_departement"],axis=1)
 
 # datalicenses = datalicenses.replace({"'": ""}, regex=True)
@@ -57,13 +58,11 @@ data_clubs2=data_clubs2.astype("float64")
 # result.to_csv("licenses_by_year_region_fed.csv", index=False)
 conn=dbc.TryConnect()
 
-#for i,row in datamedia.iterrows():
-    #quer="INSERT INTO media_table.media_data (sport_event,sport,year,avrg_tv_aud,avrg_tv_match,total_match,hours_live,numb_of_post) " \
-    #"VALUES ('"+row["sport_event"]+"','"+row["sport"]+"','"+str(row["year"])+"','"+str(row["avrg_tv_aud"])+"','"+str(row["avrg_tv_match"])+"',"
-    #"'"+str(row["total_match"])+"','"+str(row["hours_live"])+"','"+str(row["numb_of_post"])+"')"    
-    #print(quer)
-    #conn._execute_query(quer)
-    #conn.commit()
+for i,row in datamedia.iterrows():
+    quer="INSERT INTO media_table.media_data (sport_event,sport,year,avrg_tv_aud,avrg_tv_match,total_match,hours_live,numb_of_post,genre) VALUES ('"+row["sport_event"]+"','"+row["sport"]+"','"+str(row["year"])+"','"+str(row["avrg_tv_aud"])+"','"+str(row["avrg_tv_match"])+"','"+str(row["total_match"])+"','"+str(row["hours_live"])+"','"+str(row["numb_of_post"])+"','"+str(row["genre"])+"')"    
+    print(quer)
+    conn._execute_query(quer)
+    conn.commit()
 
 # for i,row in result.iterrows():
 #     quer="INSERT INTO media_table.licenses (year,  region,  nom_fed, total_lic, total_f, total_h, h_1_9, h_10_19, h_20_29,h_30_59,h_60_74, h_75, f_1_9, f_10_19, f_20_29, f_30_59, f_60_74, f_75) VALUES ('"+str(row["annee"])+"','"+str(row["region"])+"','"+str(row["nom_fed"])+"','"+str(row["total_license"])+"','"+str(row["total_f"])+"','"+str(row["total_h"])+"','"+str(row["h_1_9"])+"','"+str(row["h_10_19"])+"','"+str(row["h_20_29"])+"','"+str(row["h_30_59"])+"','"+str(row["h_60_74"])+"','"+str(row["h_75"])+"','"+str(row["f_1_9"])+"','"+str(row["f_10_19"])+"','"+str(row["f_20_29"])+"','"+str(row["f_30_59"])+"','"+str(row["f_60_74"])+"','"+str(row["f_75"])+"')"   
