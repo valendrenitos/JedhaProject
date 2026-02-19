6a82dbfc4375f460f948e2e4d12078254e59b2e8
neronalissiane <neron.alissiane@gmail.com>
2026-02-19 12:23:34 +0100

    modify

--- pages/10_rollingevol_total_lics.py
+++ pages/10_rollingevol_total_lics.py
@@ -7,7 +7,7 @@ df =mn.data1
 
 
 rolling_avg = (
-    df.groupby(["nom_fed", "year"])["total_lic"].sum()
+   df.groupby(["year", "nom_fed"])["total_lic"].sum()
 ).unstack("year").rolling(3, axis=1, min_periods=1).mean()
 
 df_long = (
@@ -15,7 +15,7 @@ df_long = (
     .mul(100)
     .rename_axis("federation")
     .reset_index()
-    .melt(id_vars="federation", var_name="annee", value_name="total lics")
+    .melt(id_vars="federation", var_name="annee", value_name="total_lic")
 )
 
 df_long["progression"] = (
@@ -23,9 +23,11 @@ df_long["progression"] = (
     .transform(lambda s: s - s.iloc[0])
 )
 
+print(df_long[df_long["annee"]==2020])
+
 fig = px.scatter(
     df_long,
-    x="total lics",
+    x="total_lic",
     y="progression",
     animation_frame="annee",
     animation_group="federation",
@@ -42,7 +44,7 @@ fig = px.scatter(
 fig.add_vline(x=50, line_dash="dash", line_color="red", opacity=0.5)
 fig.add_hline(y=0, line_dash="dash", line_color="red", opacity=0.5)
 
-fig.update_xaxes(range=[0, 100])
+fig.update_xaxes(range=[df["total_lic"].min(), df["total_lic"].max()])
 fig.update_yaxes(range=[-10, 35])
 
 st.plotly_chart(fig, use_container_width=True)
